428db2f27da2b88886e23828969f64db5e4afd85
alexbay66 <bayard7@yahoo.com>
2022-02-28 16:03:08 +0100

    creation du repo
    ajout des premiers chapitres de cours.
    ajout des exo correspondants.

---
+++ hello.py
@@ -0,0 +1,4 @@
+hello.py
+nombre = 123
+print(nombre)
+
---
+++ variables.py
@@ -0,0 +1,38 @@
+# ceci un commentaire
+
+
+
+# foo
+# bar
+# baz
+
+
+nombre = 123
+print(nombre)
+
+nombre = 3.14
+print(nombre)
+
+nombre = "123"
+print(nombre)
+
+text = "lorem ipsum"
+print(text)
+
+is_day =  True
+print(is_day)
+
+has_sugar = False
+print(has_sugar)
+
+has_accepted_ula = None
+print(has_accepted_ula)
+
+html_code = '<h1>titre de mon blog</1>'
+print(html_code)
+
+nickname = "john \"dead\" doe"
+print(nickname)
+
+multiline_text = "foo\nbar\nbaz"
+print(multiline_text)
